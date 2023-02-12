3bb5a6d51dbec2f9ba21959af763b61c4ee11d89
TaiLee <letuantai15f@gmail.com>
2023-02-12 21:26:16 +0700

    login signup

--- src/controller/Tai-Khoan/tai-khoan.controller.js
+++ src/controller/Tai-Khoan/tai-khoan.controller.js
@@ -2,10 +2,26 @@ const { Op } = require("sequelize");
 const taiKhoanService=require('../../services/tai-khoan/tai-khoan.service')
 const createTaiKhoan=async(req,res)=>{
     try {
+        const checkTaikhoan=await taiKhoanService.getTaiKhoan(req.body)
+        if(checkTaikhoan){
+            return res.status(200).json({message:"Tài khoản đã tồn tại"})
+        }
         const tai_khoan=await taiKhoanService.createTaiKhoan(req.body)
         return res.status(200).json(tai_khoan)
     } catch (error) {
         console.log(error)
     }
 }
-module.exports={createTaiKhoan}
+const dangNhap=async(req,res)=>{
+    try {
+        const tai_khoan=await taiKhoanService.dangNhap(req.body)
+        if(tai_khoan){
+            return res.status(200).json({message:"Success"})
+        }else{
+            return res.status(200).json({message:"Fail"})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports={createTaiKhoan,dangNhap}
