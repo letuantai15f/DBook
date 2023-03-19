fb5555f03347a235cec17ae1a10f168ba3d6d9c5
TaiLee <letuantai15f@gmail.com>
2023-03-19 14:03:17 +0700

    sw

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors=require('cors')
 const exphbs = require("express-handlebars");
 const path = require("path");
 var bodyParser = require("body-parser");
@@ -7,8 +8,41 @@ const app = express();
 const port = 3000;
 require("dotenv").config();
 const api = require("./routes/api");
-const routerAuth = require("./routes/auth/Auth");
-const routerHome = require("./routes/home/Home");
+const { log } = require("console");
+
+// swap
+
+const expressSwagger = require("express-swagger-generator")(app);
+let options = {
+    swaggerDefinition: {
+        openapi: "2.0.0",
+        info: {
+            description: "Api Dbok",
+            title: "Real Estate",
+            version: "1.0.0",
+        },
+        host: process.env.SWAGGER_HOST,
+        basePath: "/api",
+        produces: ["application/json", "application/xml"],
+        schemes: ["http", "https"],
+        securityDefinitions: {
+            JWT: {
+                type: "apiKey",
+                in: "header",
+                name: "Authorization",
+                description: "",
+            },
+        },
+        response: {
+            200: {
+                message: "success",
+            },
+        },
+    },
+    basedir: __dirname, 
+    files: ["./routes/**/*.js", "./models/**/*.js", "./api/**/*.js", "./controller/**/index.js"], //Path to the API handle folder
+};
+expressSwagger(options);
 
 // Template engine
 app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
@@ -16,17 +50,14 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors())
 
-
-// HTTP logger
-// app.use(morgan("combined"));
 
 //Middleware
 // app.use(express.urlencoded());
 
 app.use(express.json());
 app.use("/api", api);
-app.use("/auth", routerAuth);
-app.use("/", routerHome);
 
+console.log(__dirname);
 app.listen(port);
--- src/services/tai-khoan/tai-khoan.service.js
+++ src/services/tai-khoan/tai-khoan.service.js
@@ -4,25 +4,25 @@ const BacSi = require('../../models/BacSi/bac-si.model')
 const NhanVien = require('../../models/NhanVien/nhan-vien.model')
 const BenhNhan = require('../../models/BenhNhan/benh-nhan.model')
 const jwt = require('jsonwebtoken');
-const bcrypt=require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const createTaiKhoan = async (data) => {
     try {
-        
-        const mat_khau=changePass(data.mat_khau)
-        if(mat_khau){
-            const taiKhoan= await TaiKhoan.create({
-            tai_khoan: data.tai_khoan,
-            mat_khau: mat_khau,
-            quyen: data.quyen,
-            trang_thai: "Created"
-           
-        })
-        return taiKhoan;
-    } 
-        
+
+        const mat_khau = changePass(data.mat_khau)
+        if (mat_khau) {
+            const taiKhoan = await TaiKhoan.create({
+                tai_khoan: data.tai_khoan,
+                mat_khau: mat_khau,
+                quyen: data.quyen,
+                trang_thai: "Created"
+
+            })
+            return taiKhoan;
+        }
+
     } catch (error) {
-       console.log(error)
+        console.log(error)
     }
 }
 const login = async (data) => {
@@ -30,12 +30,12 @@ const login = async (data) => {
 
         const taiKhoan = await TaiKhoan.findOne({ where: { tai_khoan: data.tai_khoan } })
         if (taiKhoan) {
-            let check=bcrypt.compareSync(data.mat_khau, taiKhoan.mat_khau)
+            let check = bcrypt.compareSync(data.mat_khau, taiKhoan.mat_khau)
             if (check) {
                 let user;
                 switch (taiKhoan.quyen) {
                     case 1:
-                        user=taiKhoan
+                        user = taiKhoan
                         break;
                     case 2:
                         user = await BacSi.findOne({ where: { tai_khoan_id: taiKhoan.id } })
@@ -49,15 +49,15 @@ const login = async (data) => {
                     default:
                         break;
                 }
-                const payload = { id: user.id,quyen:taiKhoan.quyen};
+                const payload = { id: user.id, quyen: taiKhoan.quyen };
                 const token = jwt.sign(payload, process.env.JWT_SECRET);
-                return {token:token,userID:user.id}
+                return { token: token, userID: user.id }
             }
             else { return false }
-        }else{
-          return false;  
+        } else {
+            return false;
         }
-        
+
     } catch (error) {
         console.log(error);
     }
@@ -65,7 +65,7 @@ const login = async (data) => {
 const getTaiKhoan = async (data) => {
     return await TaiKhoan.findOne({ where: { tai_khoan: data.tai_khoan } })
 }
-const changePass =  (passWord) => {
+const changePass = (passWord) => {
     var salt = bcrypt.genSaltSync(10)
     var hash = bcrypt.hashSync(passWord, salt)
     return hash
