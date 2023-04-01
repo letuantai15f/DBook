6cb7755c8977722065b587426206763ab435e8e1
TaiLee <letuantai15f@gmail.com>
2023-04-01 16:17:29 +0700

    update

--- src/services/lich-dat/lich-dat.service.js
+++ src/services/lich-dat/lich-dat.service.js
@@ -1,32 +1,47 @@
-const LichDat=require('../../models/LichDat/lich-dat.model')
+const LichDat = require('../../models/LichDat/lich-dat.model')
 
-const getLichDat=async(id)=>{
+const getLichDat = async (id) => {
     try {
-        return await LichDat.findAll({where:{benh_nhan_id:id}})
+        return await LichDat.findAll({ where: { benh_nhan_id: id } })
     } catch (error) {
         console.log(error);
     }
 }
-const createLichDat=async(data)=>{
+const createLichDat = async (data) => {
     try {
         return await LichDat.create(data)
     } catch (error) {
         console.log(error)
     }
 }
-const deleteLichDat=async(id)=>{
+const deleteLichDat = async (id) => {
     try {
-        return await LichDat.update({trang_thai:"delete"},{where:{id}})
+        return await LichDat.update({ trang_thai: "delete" }, { where: { id } })
     } catch (error) {
         console.log();
     }
 }
-const updateLichDat=async(id,data)=>{
+const updateLichDat = async (id, data) => {
     try {
-        return await LichDat.update(data,{where:{id}})
+        return await LichDat.update(data, { where: { id } })
     } catch (error) {
         console.log();
     }
+}
+const findLichDatId = async (id) => {
+    try {
+        const lichDat = await LichDat.findOne({ where: { id } })
+        if (lichDat) {
+            return lichDat;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+const getAllLichDat=async(where)=>{
+    return await LichDat.findAll({where})
 }
 
-module.exports={createLichDat,deleteLichDat,updateLichDat,getLichDat}
+module.exports = { createLichDat, deleteLichDat, updateLichDat, getLichDat,findLichDatId,getAllLichDat }
