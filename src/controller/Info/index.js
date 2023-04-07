ba1341ea8715ae3431774a1f391b11bc76916351
TaiLee <letuantai15f@gmail.com>
2023-04-07 13:46:09 +0700

    test CICD

--- src/controller/Info/index.js
+++ src/controller/Info/index.js
@@ -19,13 +19,7 @@ router.get('/khoa',khoaController.getKhoa)
  * @returns {Error}  default - Unexpected error
  */
 router.get('/bac-si',bacSiController.getBacSi)
-/**
- * @route get /info/bac
- * @group info 
- * @returns {object} 200
- * @returns {Error}  default - Unexpected error
- */
-router.get('/bac-si',bacSiController.getBacSi)
+
 
 
 module.exports=router
