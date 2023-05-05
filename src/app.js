f639086ba71b788f3ee7794daeef85c7f733761c
TaiLee <letuantai15f@gmail.com>
2023-05-05 21:51:06 +0700

    fix domain

--- src/app.js
+++ src/app.js
@@ -59,6 +59,13 @@ app.use(
     extended: true,
   })
 );
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+app.use(allowCrossDomain);
 
 const server = http.createServer(app);
 global.io = new Server(server, {
